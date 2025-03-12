47eea32867a91580bbd99ccc974a6ac55ceca53e
jayveer-singh1 <jayveersinghrawat93512@gmail.com>
2025-03-12 22:24:07 +0530

    Fourth Commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20'>
+    <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-5 border border-b-4'>
       <img width={160} src={assets.logo} alt="" />
       <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @Jayveer Singh | All right reserved.</p>
       <div className='flex gap-2.5'>
